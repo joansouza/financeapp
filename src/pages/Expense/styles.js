7004e3fdb112f3d2daa4291b321ee69f4a4f7698
joan pereira <joanpereira@edu.unifor.br>
2022-04-18 02:13:49 -0300

    css

--- src/pages/Expense/styles.js
+++ src/pages/Expense/styles.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { ContainerApp } from '../../styles/GlobalStyles';
+
 
 export const Container = styled.div``;
 export const AddButton = styled.div`
@@ -23,29 +23,4 @@ export const Appdiv = styled.div`
 `;
 export const GeneralInfo = styled.div`
   display: flex;
-`;
-export const Content = styled(ContainerApp)`
-  h1 {
-    padding-top: 20px;
-  }
-  width: 300px;
-  max-width: 100%;
-  border-radius: 10px;
-  background: #672beb;
-  color: #fff;
-`;
-export const DescriptionBar = styled(ContainerApp)`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
-  max-width: 100%;
-  margin: 10px 60px 0 60px;
-  background: #672beb;
-  color: #fff;
-  border-radius: 20px;
-  gap: 30px;
-  h1 {
-    font-family: sans-serif;
-    font-weight: lighter;
-    font-size: 20px;
-  }
 `;
--- src/pages/Income/components/ShowIncomes/styles.js
+++ src/pages/Income/components/ShowIncomes/styles.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const DescriptionBar = styled.div`
+padding:25px ;
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
   max-width: 100%;
